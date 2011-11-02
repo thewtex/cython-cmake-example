d1ceba806206f25544dbb1147fda85860d2a337f
Matt McCormick (thewtex) <matt@mmmccormick.com>
2011-11-02 00:52:38 -0400

    Add basic setup.py.

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+from distutils.sysconfig import get_python_lib
+import glob
+
+
+setup(
+    name = "cythoncmakeexample",
+    package_dir = {'': 'src'},
+    data_files = [(get_python_lib(), glob.glob('src/*.so'))],
+    author = 'Matt McCormick',
+    description = 'Use the CMake build system to make Cython modules.',
+    license = 'Apache',
+    keywords = 'cmake cython build',
+    url = 'http://github.com/thewtex/cython-cmake-example',
+    test_require = ['nose'],
+    zip_safe = False,
+    )
